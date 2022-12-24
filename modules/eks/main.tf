d149479775b7340ccfad356bcbe07de8fbcf61c6
root <root@DESKTOP-2F0PK4E.localdomain>
2022-12-24 13:09:36 +0100

    eks cloudwatch insights

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -214,279 +214,279 @@ resource "aws_eks_node_group" "node" {
 # Cloudwatch Agent
 ##################
 
-resource "aws_iam_role" "sa_role" {
-  count = var.create_eks_node_group_role ? 1 : 0
-  name  = "role-service-account"
+//resource "aws_iam_role" "sa_role" {
+//  count = var.create_eks_node_group_role ? 1 : 0
+//  name  = "role-service-account"
+//
+//  tags               = var.tags
+//  assume_role_policy = <<POLICY
+//{
+//  "Version": "2012-10-17",
+//  "Statement": [
+//    {
+//      "Effect": "Allow",
+//      "Principal": {
+//        "Federated": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${trim(aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer, "htpps://")}"
+//      },
+//      "Action": "sts:AssumeRoleWithWebIdentity",
+//      "Condition": {
+//        "StringEquals": {
+//          "${trim(aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer, "htpps://")}:sub": "sts.amazonaws.com",
+//          "${trim(aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer, "htpps://")}:aud": "system:serviceaccount:${kubernetes_namespace.ns.metadata[0].name}:${kubernetes_service_account.sa.metadata[0].name}"
+//        }
+//      }
+//    }
+//  ]
+//}
+//POLICY
+//}
 
-  tags               = var.tags
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Federated": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${trim(aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer, "htpps://")}"
-      },
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringEquals": {
-          "${trim(aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer, "htpps://")}:sub": "sts.amazonaws.com",
-          "${trim(aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer, "htpps://")}:aud": "system:serviceaccount:${kubernetes_namespace.ns.metadata[0].name}:${kubernetes_service_account.sa.metadata[0].name}"
-        }
-      }
-    }
-  ]
-}
-POLICY
-}
 
-
-resource "kubernetes_namespace" "ns" {
-  metadata {
-    labels = {
-      name = "amazon-cloudwatch"
-    }
-    name = "amazon-cloudwatch"
-  }
-  //    depends_on = [aws_eks_node_group.node]
-}
-
-resource "kubernetes_service_account" "sa" {
-  metadata {
-    name      = "cloudwatch-agent"
-    namespace = kubernetes_namespace.ns.metadata[0].name
-    annotations = {
-      "eks.amazonaws.com/role-arn" = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/role-service-account"
-    }
-  }
-
-}
-
-resource "kubernetes_cluster_role" "cr" {
-  metadata {
-    name = "cloudwatch-agent-role"
-  }
-
-  rule {
-    api_groups = [""]
-    resources  = ["pods", "nodes", "endpoints"]
-    verbs      = ["list", "watch"]
-  }
-  rule {
-    api_groups = ["apps"]
-    resources  = ["replicasets"]
-    verbs      = ["list", "watch"]
-  }
-  rule {
-    api_groups = ["batch"]
-    resources  = ["jobs"]
-    verbs      = ["list", "watch"]
-  }
-  rule {
-    api_groups = [""]
-    resources  = ["nodes/proxy"]
-    verbs      = ["get"]
-  }
-  rule {
-    api_groups = [""]
-    resources  = ["nodes/stats", "configmaps", "events"]
-    verbs      = ["create"]
-  }
-  rule {
-    api_groups     = [""]
-    resources      = ["configmaps"]
-    resource_names = ["cwagent-clusterleader"]
-    verbs          = ["get", "update"]
-  }
-}
-
-resource "kubernetes_cluster_role_binding" "crb" {
-  metadata {
-    name = "cloudwatch-agent-role-binding"
-  }
-
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = kubernetes_cluster_role.cr.metadata[0].name
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = kubernetes_service_account.sa.metadata[0].name
-    namespace = kubernetes_namespace.ns.metadata[0].name
-  }
-}
-data "aws_region" "current" {}
-
-data "template_file" "this" {
-  template = file("${path.module}/templates/config.tpl")
-  vars = {
-    region_name  = data.aws_region.current.name
-    cluster_name = aws_eks_cluster.eks_cluster.name
-  }
-}
-
-resource "kubernetes_config_map" "cm" {
-  metadata {
-    name      = "cwagentconfig"
-    namespace = kubernetes_namespace.ns.metadata[0].name
-  }
-  data = {
-    "cwagentconfig.json" = data.template_file.this.rendered
-  }
-
-}
-
-resource "kubernetes_daemonset" "this" {
-  metadata {
-    name      = "cloudwatch-agent"
-    namespace = kubernetes_namespace.ns.metadata[0].name
-  }
-
-  spec {
-    selector {
-      match_labels = {
-        name = "cloudwatch-agent"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          name = "cloudwatch-agent"
-        }
-      }
-      spec {
-        container {
-          name  = "cloudwatch-agent"
-          image = "amazon/cloudwatch-agent:1.247354.0b251981"
-          port {
-            container_port = 8125
-            host_port      = 8125
-            protocol       = "UDP"
-          }
-          resources {
-            limits = {
-              cpu    = "200m"
-              memory = "200Mi"
-            }
-            requests = {
-              cpu    = "200m"
-              memory = "200Mi"
-            }
-          }
-          env {
-            name = "HOST_IP"
-            value_from {
-              field_ref {
-                field_path = "status.hostIP"
-              }
-            }
-          }
-          env {
-            name = "HOST_NAME"
-            value_from {
-              field_ref {
-                field_path = "spec.nodeName"
-              }
-            }
-          }
-          env {
-            name = "K8S_NAMESPACE"
-            value_from {
-              field_ref {
-                field_path = "metadata.namespace"
-              }
-            }
-          }
-          env {
-            name  = "CI_VERSION"
-            value = "k8s/1.3.11"
-          }
-          volume_mount {
-            mount_path = "/etc/cwagentconfig"
-            name       = "cwagentconfig"
-          }
-          volume_mount {
-            mount_path = "/rootfs"
-            name       = "rootfs"
-            read_only  = true
-          }
-          volume_mount {
-            mount_path = "/var/run/docker.sock"
-            name       = "dockersock"
-            read_only  = true
-          }
-          volume_mount {
-            mount_path = "/var/lib/docker"
-            name       = "varlibdocker"
-            read_only  = true
-          }
-          volume_mount {
-            mount_path = "/run/containerd/containerd.sock"
-            name       = "containerdsock"
-            read_only  = true
-          }
-          volume_mount {
-            mount_path = "/sys"
-            name       = "sys"
-            read_only  = true
-          }
-          volume_mount {
-            mount_path = "/dev/disk"
-            name       = "devdisk"
-            read_only  = true
-          }
-        }
-        volume {
-          name = "cwagentconfig"
-          config_map {
-            name = kubernetes_config_map.cm.metadata[0].name
-          }
-        }
-        volume {
-          name = "rootfs"
-          host_path {
-            path = "/"
-          }
-        }
-        volume {
-          name = "dockersock"
-          host_path {
-            path = "/var/run/docker.sock"
-          }
-        }
-        volume {
-          name = "varlibdocker"
-          host_path {
-            path = "/var/lib/docker"
-          }
-        }
-        volume {
-          name = "containerdsock"
-          host_path {
-            path = "/run/containerd/containerd.sock"
-          }
-        }
-        volume {
-          name = "sys"
-          host_path {
-            path = "/sys"
-          }
-        }
-        volume {
-          name = "devdisk"
-          host_path {
-            path = "/dev/disk"
-          }
-        }
-        termination_grace_period_seconds = 60
-        service_account_name             = kubernetes_service_account.sa.metadata[0].name
-      }
-    }
-  }
-}
+//resource "kubernetes_namespace" "ns" {
+//  metadata {
+//    labels = {
+//      name = "amazon-cloudwatch"
+//    }
+//    name = "amazon-cloudwatch"
+//  }
+//  //    depends_on = [aws_eks_node_group.node]
+//}
+//
+//resource "kubernetes_service_account" "sa" {
+//  metadata {
+//    name      = "cloudwatch-agent"
+//    namespace = kubernetes_namespace.ns.metadata[0].name
+//    annotations = {
+//      "eks.amazonaws.com/role-arn" = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/role-service-account"
+//    }
+//  }
+//
+//}
+//
+//resource "kubernetes_cluster_role" "cr" {
+//  metadata {
+//    name = "cloudwatch-agent-role"
+//  }
+//
+//  rule {
+//    api_groups = [""]
+//    resources  = ["pods", "nodes", "endpoints"]
+//    verbs      = ["list", "watch"]
+//  }
+//  rule {
+//    api_groups = ["apps"]
+//    resources  = ["replicasets"]
+//    verbs      = ["list", "watch"]
+//  }
+//  rule {
+//    api_groups = ["batch"]
+//    resources  = ["jobs"]
+//    verbs      = ["list", "watch"]
+//  }
+//  rule {
+//    api_groups = [""]
+//    resources  = ["nodes/proxy"]
+//    verbs      = ["get"]
+//  }
+//  rule {
+//    api_groups = [""]
+//    resources  = ["nodes/stats", "configmaps", "events"]
+//    verbs      = ["create"]
+//  }
+//  rule {
+//    api_groups     = [""]
+//    resources      = ["configmaps"]
+//    resource_names = ["cwagent-clusterleader"]
+//    verbs          = ["get", "update"]
+//  }
+//}
+//
+//resource "kubernetes_cluster_role_binding" "crb" {
+//  metadata {
+//    name = "cloudwatch-agent-role-binding"
+//  }
+//
+//  role_ref {
+//    api_group = "rbac.authorization.k8s.io"
+//    kind      = "ClusterRole"
+//    name      = kubernetes_cluster_role.cr.metadata[0].name
+//  }
+//  subject {
+//    kind      = "ServiceAccount"
+//    name      = kubernetes_service_account.sa.metadata[0].name
+//    namespace = kubernetes_namespace.ns.metadata[0].name
+//  }
+//}
+//data "aws_region" "current" {}
+//
+//data "template_file" "this" {
+//  template = file("${path.module}/templates/config.tpl")
+//  vars = {
+//    region_name  = data.aws_region.current.name
+//    cluster_name = aws_eks_cluster.eks_cluster.name
+//  }
+//}
+//
+//resource "kubernetes_config_map" "cm" {
+//  metadata {
+//    name      = "cwagentconfig"
+//    namespace = kubernetes_namespace.ns.metadata[0].name
+//  }
+//  data = {
+//    "cwagentconfig.json" = data.template_file.this.rendered
+//  }
+//
+//}
+//
+//resource "kubernetes_daemonset" "this" {
+//  metadata {
+//    name      = "cloudwatch-agent"
+//    namespace = kubernetes_namespace.ns.metadata[0].name
+//  }
+//
+//  spec {
+//    selector {
+//      match_labels = {
+//        name = "cloudwatch-agent"
+//      }
+//    }
+//
+//    template {
+//      metadata {
+//        labels = {
+//          name = "cloudwatch-agent"
+//        }
+//      }
+//      spec {
+//        container {
+//          name  = "cloudwatch-agent"
+//          image = "amazon/cloudwatch-agent:1.247354.0b251981"
+//          port {
+//            container_port = 8125
+//            host_port      = 8125
+//            protocol       = "UDP"
+//          }
+//          resources {
+//            limits = {
+//              cpu    = "200m"
+//              memory = "200Mi"
+//            }
+//            requests = {
+//              cpu    = "200m"
+//              memory = "200Mi"
+//            }
+//          }
+//          env {
+//            name = "HOST_IP"
+//            value_from {
+//              field_ref {
+//                field_path = "status.hostIP"
+//              }
+//            }
+//          }
+//          env {
+//            name = "HOST_NAME"
+//            value_from {
+//              field_ref {
+//                field_path = "spec.nodeName"
+//              }
+//            }
+//          }
+//          env {
+//            name = "K8S_NAMESPACE"
+//            value_from {
+//              field_ref {
+//                field_path = "metadata.namespace"
+//              }
+//            }
+//          }
+//          env {
+//            name  = "CI_VERSION"
+//            value = "k8s/1.3.11"
+//          }
+//          volume_mount {
+//            mount_path = "/etc/cwagentconfig"
+//            name       = "cwagentconfig"
+//          }
+//          volume_mount {
+//            mount_path = "/rootfs"
+//            name       = "rootfs"
+//            read_only  = true
+//          }
+//          volume_mount {
+//            mount_path = "/var/run/docker.sock"
+//            name       = "dockersock"
+//            read_only  = true
+//          }
+//          volume_mount {
+//            mount_path = "/var/lib/docker"
+//            name       = "varlibdocker"
+//            read_only  = true
+//          }
+//          volume_mount {
+//            mount_path = "/run/containerd/containerd.sock"
+//            name       = "containerdsock"
+//            read_only  = true
+//          }
+//          volume_mount {
+//            mount_path = "/sys"
+//            name       = "sys"
+//            read_only  = true
+//          }
+//          volume_mount {
+//            mount_path = "/dev/disk"
+//            name       = "devdisk"
+//            read_only  = true
+//          }
+//        }
+//        volume {
+//          name = "cwagentconfig"
+//          config_map {
+//            name = kubernetes_config_map.cm.metadata[0].name
+//          }
+//        }
+//        volume {
+//          name = "rootfs"
+//          host_path {
+//            path = "/"
+//          }
+//        }
+//        volume {
+//          name = "dockersock"
+//          host_path {
+//            path = "/var/run/docker.sock"
+//          }
+//        }
+//        volume {
+//          name = "varlibdocker"
+//          host_path {
+//            path = "/var/lib/docker"
+//          }
+//        }
+//        volume {
+//          name = "containerdsock"
+//          host_path {
+//            path = "/run/containerd/containerd.sock"
+//          }
+//        }
+//        volume {
+//          name = "sys"
+//          host_path {
+//            path = "/sys"
+//          }
+//        }
+//        volume {
+//          name = "devdisk"
+//          host_path {
+//            path = "/dev/disk"
+//          }
+//        }
+//        termination_grace_period_seconds = 60
+//        service_account_name             = kubernetes_service_account.sa.metadata[0].name
+//      }
+//    }
+//  }
+//}
 
 # #########
 # EKS Addon
